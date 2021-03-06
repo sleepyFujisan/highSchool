6ad4d6dadc136a3483949dc3794a0a9f4b5b4255
sleepyFujisan <66394321+sleepyFujisan@users.noreply.github.com>
2021-03-06 07:31:00 -0800

    Update lesson4.java

--- bluePelicanJava/Lesson4/lesson4.java
+++ bluePelicanJava/Lesson4/lesson4.java
@@ -2,91 +2,63 @@
  * Text:  Blue Pelican Java                       Author:  sleepyFuji
  * Version:  001                                  Date:  Feb 12, 2019
  *
- * Purpose:  These are my answers to the lesson 4 questions: 1, 2, 4, 5, 8, 11, 12, 13
+ * Purpose:  These are my answers to the lesson 4 questions: 1, 2, 3, 5, 8, 11, 12, 13
  *
  * Course:   Computer Science 201                Teacher:  [redacted]
  * School:   [redacted]
  * Language: Java J2SE 6.0                       Target OS: Java Virtual Machine
  * System:   Intel Celeron 3GHz, Windows XP      IDE: Eclipse 3.4
  * ======================================================================= */
-package Lesson4;
-public class lesson4{;					QUESTION 1:
-/*		public static void main(String[]args){
-	int h = 103;
-	int p = 5;
-			System.out.println(h);
-			System.out.println(++h + p);
-	//Output: 109, and 104 on two different lines
-*/	/*							QUESTION 2:
-		public static void main(String[]args){
-	int j = 1;{
-		j++;
-			System.out.println(j);
-			}}
-	//Output: 2
- */	/*							
-		public static void main(String[]args){
-	int j = 1;{
-		j += 1;
-			System.out.println(j);
-	//Output: 2
-}}
-*/ /*								
-		public static void main(String[]args){
-	int z = 1;
-	int j = 1 + z++;
-			System.out.println(j);
-		}
-	//Output: 2
-*/ /*								QUESTION 3:
-		public static void main(String[]args){
-	double def;
-	double f = 1992.37;
-	def = f;
-			System.out.println(def);
-	//Output: 1993.37
-	 }
-*/ /*								QUESTION 5:
-		public static void main(String[]args){
-	int a = 100;
-	int b = 200;
-		b/=a;
-		System.out.println(b + 1);
-	}
-	//Output: 3
-*/ /*								QUESTION 8:
-		public static void main(String[]args){
-	int p = 40;
-	int q = 4;
-				System.out.println(2 + 8 * q / 2 - p);
-	}
-	//Output: -22
-*/	/*							QUESTION 11:
-		public static void main(String[]args){
-	double m = 3.14,b = 3.14,f = 3.14;
+public class lesson4{
+	
+	public static void main(String[]args){
+		// Problem #1
+		int h = 103;
+		int p = 5;
+		System.out.print("\n");
+		System.out.println(++h + p);
+		System.out.println(h);
 		
-				System.out.println(m);
-				System.out.println(b);
-				System.out.println(f);
-	}
-	/*Output: 3.14
-	//		  3.14
-	/		  3.14
-	 *
-*/ /*								QUESTION 12:
-		public static void main(String[]args){
-	int x,y,z; 
-	}
-	//This is just question 12.
-	 * 
-
-*/ /*								QUESTION 13:
-		public static void main(String[]args){
-	int m = 36;
-	int j = 5;
-	m = m / j;
-				System.out.println(m);
-	//Output:7
-	}
-*/
+		// Problem #2
+		int j = 5 ;
+		System.out.print("\n");
+		System.out.println(++j);
+		System.out.println(j++);
+		System.out.println(j += 1);
+		
+		// Problem #3
+		double def;
+		double f = 1_992.37;
+		def = f;
+		System.out.print("\n");
+		System.out.println(def);
+		
+		// Problem #5
+		int a = 100;
+		int b = 200;
+		b /= a;
+		System.out.print("\n");
+		System.out.println(b+1);
+		
+		// Problem #8
+		p = 40;
+		int q = 4;
+		System.out.print("\n");
+		System.out.println(2 + 8 * q / 2 - p);
+		
+		// Problem #11
+		double m = 3.14, b1 = 3.14, f1 = 3.14;
+		System.out.print("\n");
+		System.out.println(m + b1 + f1);
+		
+		// Problem #12
+		//int x1, y, z;
+		
+		// Problem #13
+		int m1 = 36;
+		int j1 = 5;
+		m1 = m1 / j1;
+		System.out.print("\n");
+		System.out.println(m1);
+	}	
 }
