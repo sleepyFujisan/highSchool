db7730b4b8d50cc5095bf5feed516e13af4dfecc
sleepyFujisan <66394321+sleepyFujisan@users.noreply.github.com>
2021-03-06 20:46:47 -0800

    Create lesson8.java

---
+++ bluePelicanJava/Lesson8/lesson8.java
@@ -0,0 +1,37 @@
+/* =======================================================================
+ * Text:  Blue Pelican Java                       Author:  sleepyFuji
+ * Version:  001                                  Date:  Feb 12, 2019
+ *
+ * Purpose:  Answers to questions 1-5, 7-10
+ *
+ * Course:   Computer Science 201                Teacher:  [redacted]
+ * School:   [redacted]
+ * Language: Java J2SE 6.0                       Target OS: Java Virtual Machine
+ * System:   Intel Celeron 3GHz, Windows XP      IDE: Eclipse 3.4
+ * ======================================================================= */
+
+//Questions 1-5
+//  1. True
+//  2. True
+//  3. True
+//  4. False
+//  5. True
+
+public class lesson8{
+   public static void main(String[]args){
+     //Question 7
+     boolean b = ((p) && (q));
+     System.out.println(b);
+     //Question 8
+     boolean w = ((0 < x) || (y == x));
+     System.out.println(w);
+     //Question 9
+     // The two possible values of a boolean variable are: True and False
+     //Question 10
+     boolean kDog = true;
+     if (a != b){
+      System.out.println(kDog); 
+     }
+     
+   }
+}
