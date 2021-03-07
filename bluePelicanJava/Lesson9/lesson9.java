005eba3d7f927d7947586e12e403d11ec5a00866
sleepyFujisan <66394321+sleepyFujisan@users.noreply.github.com>
2021-03-06 21:16:27 -0800

    Update lesson9.java

--- bluePelicanJava/Lesson9/lesson9.java
+++ bluePelicanJava/Lesson9/lesson9.java
@@ -15,18 +15,18 @@ import java.util.Scanner;
 public class Lesson9{
   public static void main(String[]args){
     //Question 14
-    String s1 = "school BUS";
-			if (s1.equals("school bus"))
-				System.out.println("Equal");
-			else
-			    System.out.println("Not equal");
+String s1 = "school BUS";
+	if (s1.equals("school bus"))
+		System.out.println("Equal");
+	else
+		System.out.println("Not equal");
     
     //Question 15
-		String s2 = "school BUS";
-			if (s2.equalsIgnoreCase("school bus"))
-				System.out.println("Equal");
-			else
-				System.out.println("Not equal");
+String s2 = "school BUS";
+	if (s2.equalsIgnoreCase("school bus"))
+		System.out.println("Equal");
+	else
+		System.out.println("Not equal");
     
     //Question 16
     int j = 19, m = 200;
@@ -38,19 +38,19 @@ public class Lesson9{
     
     //Question 17
     boolean b = ( g  != 34);
-				System.out.println(b);
+	System.out.println(b);
     
     //Question 18
     boolean b1 = ( k % 2 == 0);
-				System.out.println(b1);
+	System.out.println(b1);
     
     //Question 19
     Scanner Reader = new Scanner(System.in);
-		System.out.print("Enter your password: ");
+	System.out.print("Enter your password: ");
    	String s = Reader.nextLine();
 	    if (s.equals("Xray"))
-			  System.out.println("Password entered successfully.");
+			System.out.println("Password entered successfully.");
 		  else
-				 System.out.println("Incorrect password.");
+			System.out.println("Incorrect password.");
   }
 }
